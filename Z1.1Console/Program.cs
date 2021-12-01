9ecb4011a5dc43cf7679579bde54284a17fab923
GlebKucherik <95340898+GlebKucherik@users.noreply.github.com>
2021-12-01 16:24:39 +0300

    Add files via upload

---
+++ Z1.1Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace z1_1
+{
+    class Circle
+    {
+        private double Radius = 0.0;
+        private double Area = 0.0;
+        private double Pi = Math.PI;
+        public Circle()
+        {
+            fillArea();
+        }
+
+        public void fillArea()
+        {
+            Try:
+            Console.Write("Введите площадь: ");
+            if (Double.TryParse(Console.ReadLine(), out Area))
+            {
+                showCircle();
+            }
+            else
+            {
+                Console.Write("Попробуйте ещё раз:\n");
+                goto Try;
+            }
+        }
+        public void showCircle()
+        {
+            Radius = Math.Sqrt(Area / Pi);
+            Console.Write("R = " + Radius);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Console.ReadKey();
+        }
+    }
+}
